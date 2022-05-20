3f5e3604acab7805e65a82680addd885dadee648
Torgy <jctorge@gmail.com>
2022-05-20 15:39:52 -0500

    sql files

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const connection =  mysql.createConnection({
+    host: 'localhost',
+    port: 3001,
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'employee_db'
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO department(department_name)
+VALUES ("Sales"), ("Legal"), ("Marketing"), ("Engineering"), ("HR"), ("Accounting");
+
+INSERT INTO role(title, salary, department_id)
+VALUES ()
